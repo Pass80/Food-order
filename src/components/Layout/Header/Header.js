d05f2abcb82d71edbe892a68447a71b36cf18425
Feras <ferassaleh1980@gmail.com>
2022-07-31 18:52:26 +0200

    add some components

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+import classes from'./Header.module.css';
+import mealImage from '../../../assets/meals.jpg';
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+
+const Header = () => {
+  return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>Feras` Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealImage} alt='a Table full of food'/>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Header;
